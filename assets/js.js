4dd3807db1016392a18932a6f267c3dbc33e6d0d
Kevin Ding <Kevin Ding>
2016-09-25 16:27:51 -0400

    Sept25

--- assets/js.js
+++ assets/js.js
@@ -1,57 +1,74 @@
-/*
- * declare map as a global variable
- */
-var map;
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
 
-/*
- * use google maps api built-in mechanism to attach dom events
- */
-google.maps.event.addDomListener(window, "load", function () {
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-  /*
-   * create map
-   */
-  var map = new google.maps.Map(document.getElementById("map_div"), {
-    center: new google.maps.LatLng(40.0583, -74.4057),
-    zoom: 7,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.217923, lng: -74.746296},  
+    zoom: 8,
+    mapTypeId: 'roadmap'
   });
 
-  /*
-   * create infowindow (which will be used by markers)
-   */
-  var infoWindow = new google.maps.InfoWindow();
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  /*
-   * marker creater function (acts as a closure for html parameter)
-   */
-  function createMarker(options, html) {
-    var marker = new google.maps.Marker(options);
-    if (html) {
-      google.maps.event.addListener(marker, "click", function () {
-        infoWindow.setContent(html);
-        infoWindow.open(options.map, this);
-      });
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
     }
-    return marker;
-  }
 
-  /*
-   * add markers to map
-   */
-  var marker0 = createMarker({
-    position: new google.maps.LatLng(33.808678, -117.918921),
-    map: map,
-    icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
-  }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
 
-  var marker1 = createMarker({
-    position: new google.maps.LatLng(33.818038, -117.928492),
-    map: map
-  }, "<h1>Marker 1</h1><p>This is marker 1</p>");
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-  var marker2 = createMarker({
-    position: new google.maps.LatLng(33.803333, -117.915278),
-    map: map
-  }, "<h1>Marker 2</h1><p>This is marker 2</p>");
-});
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
